25e667fb6bc8727a7a4177d8ff5d3515888b273f
teddy89PJS <teddygadlawan@gmail.com>
2025-12-10 17:45:08 +0800

    Fullcalendar

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\DB;
+
+class UsersTableSeeder extends Seeder
+{
+  /**
+   * Run the database seeds.
+   */
+  public function run(): void
+  {
+    DB::table('users')->insert([
+      'name' => 'Your Name Here',
+      'email' => 'tbadlwanjr@dswd.gov.ph',
+      'password' => Hash::make('password123'), // default password
+      'role' => 'HR-Planning',
+      'email_verified_at' => now(),
+      'created_at' => now(),
+      'updated_at' => now(),
+    ]);
+  }
+}
